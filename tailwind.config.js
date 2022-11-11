09f90f02726afbda9383b46aeeeb82bff92a7982
AzureAD\ValentinaEgger <valentina.egger@dualbootpartners.com>
2022-11-11 12:39:36 -0300

    challenge - 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    fontFamily: {
+      sans: ['Overpass', 'sans-serif'],
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/line-clamp')
+  ],
 };
